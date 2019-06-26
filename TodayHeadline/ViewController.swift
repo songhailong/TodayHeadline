7d62ccd849e21839bc403093d0f2d03b789baaf4
songhailong <997496837@qq.com>
2019-06-26 11:43:14 +0800

    Initial Commit

---
+++ TodayHeadline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodayHeadline
+//
+//  Created by 靓萌服饰靓萌服饰 on 2019/6/26.
+//  Copyright © 2019年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TodayHeadlineTests/TodayHeadlineTests.swift
@@ -0,0 +1,34 @@
+//
+//  TodayHeadlineTests.swift
+//  TodayHeadlineTests
+//
+//  Created by 靓萌服饰靓萌服饰 on 2019/6/26.
+//  Copyright © 2019年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+import XCTest
+@testable import TodayHeadline
+
+class TodayHeadlineTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
