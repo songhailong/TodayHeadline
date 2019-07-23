f8a6527bd42dc3f5fa167ab4effcf252d84e665b
songhailong <997496837@qq.com>
2019-07-23 10:04:34 +0800

    第二次更新

--- TodayHeadline/Class/Home/Main/View/THHomeAddHeadViewCell.swift
+++ TodayHeadline/Class/Home/Main/View/THHomeAddHeadViewCell.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class THHomeAddHeadViewCell: UICollectionReusableView {
     var isEdit=false
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    
+    lazy var eidButton:UIButton = {
+        let eidButton=UIButton()
+        eidButton.setTitle("编辑", for: .normal)
+        eidButton.titleLabel?.textColor=THRGBColor(R: 207, G: 84, B: 83)
+        return eidButton
+    }()
+    
+    lazy var jonechannelBtn: UIButton = {
+        let jonechannelB=UIButton()
+        jonechannelB.setTitle("点击进入频道", for: .normal)
+        jonechannelB.titleLabel?.textColor=UIColor.gray
+        return jonechannelB
+    }()
+    lazy var channelBtn: UIButton = {
+        let channelB=UIButton()
+        channelB.setTitle("我的频道", for: .normal)
+        channelB.titleLabel?.textColor=UIColor.black
+        return channelB
+    }()
 
 }
